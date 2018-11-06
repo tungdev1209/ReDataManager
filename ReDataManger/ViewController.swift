1f1f5e7ea1fc1b92a481d6684effa77bc34d013b
TungNQ4 <tungnguyen.dev@gmail.com>
2018-11-06 22:14:31 +0700

    Initial Commit

---
+++ ReDataManger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReDataManger
+//
+//  Created by Tung Nguyen on 11/6/18.
+//  Copyright © 2018 Tung Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
