4ebbe63548eb30f5cd0b6ff1e3619dc21d60784e
TungNQ4 <tungnguyen.dev@gmail.com>
2018-11-17 16:37:11 +0700

    adjust alert

--- ReDataManger/View/MyAlert.swift
+++ ReDataManger/View/MyAlert.swift
@@ -10,13 +10,13 @@ import UIKit
 
 class MyAlert {
     private var alert: UIAlertController!
-    private var _title: String = AppName
-    private var _completion: (() -> Void)?
-    private var _animated: Bool = true
+    var title: String = Bundle.main.getAppName()
+    var completion: (() -> Void)?
+    var animated: Bool = true
     
     class func create(_ message: String?) -> MyAlert {
         let myAlert = MyAlert()
-        myAlert.alert = UIAlertController(title: myAlert._title, message: message, preferredStyle: UIAlertController.Style.alert)
+        myAlert.alert = UIAlertController(title: myAlert.title, message: message, preferredStyle: UIAlertController.Style.alert)
         return myAlert
     }
     
@@ -32,43 +32,27 @@ class MyAlert {
     }
     
     func completion(_ compl: (() -> Void)?) -> MyAlert {
-        _completion = compl
+        completion = compl
         return self
     }
     
     func animated(_ animate: Bool) -> MyAlert {
-        _animated = animate
+        animated = animate
         return self
     }
     
     func present(_ vc: UIViewController) {
         if !Thread.isMainThread {
-            DispatchQueue.main.async { [weak self] in
+            DispatchQueue.main.sync { [weak self] in
                 guard let `self` = self else {return}
                 self.present(vc)
             }
             return
         }
-        vc.present(self.alert, animated: self._animated, completion: self._completion)
+        vc.present(alert, animated: animated, completion: completion)
     }
     
     deinit {
         print("MyAlert - DEALLOC")
-    }
-}
-
-import AVFoundation
-class MyPlayerView: UIView {
-    var player: AVPlayer! {
-        get {
-            return (layer as! AVPlayerLayer).player
-        }
-        set(newPlayer) {
-            (layer as! AVPlayerLayer).player = newPlayer
-        }
-    }
-    
-    override class var layerClass: AnyClass {
-        return AVPlayerLayer.self
     }
 }
